c06ebdfb3ef3fab7dca15bde23e09ad81cec2035
Habibulloh <husanovhabibullohhusanov@gmail.com>
2024-06-02 07:16:09 +0500

    Register page design updated

--- src/components/UI/input.jsx
+++ src/components/UI/input.jsx
@@ -1,8 +1,16 @@
+import { useState } from "react";
+import Button from "./button";
+
 function Input({ type, label, ...props }) {
+    const [ typeThis, setTypeThis ] = useState(true);
     return (
-        <div>
-            <label htmlFor="" className="block pl-10 text-xl">{label}</label>
-            <input type={type} className="block w-4/5 h-12 bg-[rgba(255,255,255,0.07)] border border-[#2e2121d3] rounded-xl m-4 px-2.5 text-base text-white placeholder:text-[#e5e5e5]" {...props} />
+        <div className="mx-auto">
+            <label htmlFor="" className="text-xl">{label}</label>
+            <input type={type == "password" ? typeThis ? "password" : "text" : type} className="block outline-none w-full h-14 bg-[rgba(255,255,255,0.07)] border border-[#2e2121d3] rounded-xl mx-auto mb-6 mt-2 px-4 text-xl text-white placeholder:text-[#e5e5e5]" {...props} />
+            {type == "password"
+            ? <Button onClick={() => setTypeThis(!typeThis)}>Change</Button>
+            : null
+        }
         </div>
     )
 }
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,6 +1,5 @@
 import Button from "../components/UI/button"
 import Input from "../components/UI/input"
-import Password from "../components/UI/password";
 
 function Login() {
   const login = (e) => {
@@ -8,13 +7,15 @@ function Login() {
 
   }
   return (
-    <div className="*:outline-none *:box-border *:p-0 *:m-0">
-      <div className="flex w-full h-screen bg-[#080710] content-center items-center">
-        <form onSubmit={login} className="w-1/4 p-9 block text-white bg-[rgba(255,255,255,0.13)] backdrop-blur-md border-2 border-[rgba(255,255,255,0.13)] rounded-xl shadow-lg shadow-[rgba(8,7,16,0.6)] z-1 before:content-[''] before:w-64 before:h-64 before:bg=[linear-gradient(#4141e2, #8a66d6)]" >
+    <div className="*:outline-none *:box-border *:p-0 *:m-0 font-serif">
+      <div className="size-64 bg-gradient-to-tr from-[#4141e2] to-[#8a66d6] blur-[0.5px] rounded-full absolute top-[67%] left-[55%]"></div>
+      <div className="flex w-full items-center h-screen bg-[#080710]">
+        <form onSubmit={login} className="min-w-[480px] py-9 px-12 m-auto block text-white bg-[rgba(255,255,255,0.13)] backdrop-blur-md border-2 border-[rgba(255,255,255,0.13)] rounded-xl shadow-lg shadow-[rgba(8,7,16,0.6)] z-1 before:content-[''] before:size-44 before:bg-gradient-to-tr before:from-[#8a66d6] before:to-[#4141e2] before:-ml-[140px] before:-mt-[100px] before:blur-[0.5px] before:rounded-full before:absolute" >
+          <h1 className="text-center my-8 text-white text-4xl font-bold">Sign up</h1>
           <Input type="text" label="Email" placeholder="Enter email" />
           <Input type="password" label="Password" placeholder="Enter password" />
-          <Password label="Password" placeholder="Enter password" />
-          <Button>Log in</Button>
+          <Input type="password" label="Password confirmation" placeholder="Re enter password" />
+          <Button className="w-full mx-auto mb-8 mt-4 py-4 px-11 bg-white border-2 rounded-xl border-white cursor-pointer text-black text-xl font-bold hover:border-black">Sign up</Button>
         </form>
       </div>
     </div>
