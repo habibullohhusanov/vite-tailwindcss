7cc976c2f42725ac4780f865cc221386b632c242
Habibulloh <husanovhabibullohhusanov@gmail.com>
2024-07-03 05:27:11 +0500

    changed some size

--- src/components/UI/input.jsx
+++ src/components/UI/input.jsx
@@ -5,10 +5,10 @@ function Input({ type, label, ...props }) {
   const [typeThis, setTypeThis] = useState(true);
   return (
     <div className="mx-auto">
-      <label htmlFor="" className="text-xl">{label}</label>
-      <input type={type == "password" ? typeThis ? "password" : "text" : type} className="block outline-none w-full h-14 bg-[rgba(255,255,255,0.07)] border border-[#2e2121d3] rounded-xl mx-auto mb-6 mt-2 px-4 text-xl text-white placeholder:text-[#e5e5e5]" {...props} />
+      <label htmlFor="" className="md:text-xl">{label}</label>
+      <input type={type == "password" ? typeThis ? "password" : "text" : type} className="block outline-none w-full h-12 md:h-14 bg-[rgba(255,255,255,0.07)] border border-[#2e2121d3] rounded-xl mx-auto mb-4 md:mb-6 mt-1 md:mt-2 px-4 md:text-xl text-white placeholder:text-[#e5e5e5]" {...props} />
       {type == "password"
-        ? <Button onClick={() => setTypeThis(!typeThis)}>Change</Button>
+        ? <Button onClick={() => setTypeThis(!typeThis)} className="text-sm md:text-base">Change</Button>
         : null
       }
     </div>
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -9,15 +9,15 @@ function Login() {
   }
   return (
     <div className="*:outline-none *:box-border *:p-0 *:m-0 font-serif select-none">
-      <div className="size-64 bg-gradient-to-tr from-[#4141e2] to-[#8a66d6] blur-[0.5px] rounded-full absolute top-[67%] left-[55%]"></div>
       <div className="flex w-full items-center h-screen bg-[#080710]">
-        <form onSubmit={login} className="min-w-[480px] py-9 px-12 m-auto block text-white bg-[rgba(255,255,255,0.13)] backdrop-blur-md border-2 border-[rgba(255,255,255,0.13)] rounded-xl shadow-lg shadow-[rgba(8,7,16,0.6)] z-1 before:content-[''] before:size-44 before:bg-gradient-to-tr before:from-[#8a66d6] before:to-[#4141e2] before:-ml-[140px] before:-mt-[100px] before:blur-[0.5px] before:rounded-full before:absolute" >
-          <h1 className="text-center my-8 text-white text-4xl font-bold">Sign up</h1>
+        <div className="size-48 md:size-64 bg-gradient-to-tr from-[#4141e2] to-[#8a66d6] blur-[0.5px] rounded-full absolute top-[70%] md:top-[67%] left-[60%] md:left-[55%]"></div>
+        <form onSubmit={login} className="min-w-[380px] md:min-w-[480px] py-4 px-9 m-auto block text-white bg-[rgba(255,255,255,0.13)] backdrop-blur-md border-2 border-[rgba(255,255,255,0.13)] rounded-xl shadow-lg shadow-[rgba(8,7,16,0.6)] z-1 before:content-[''] before:size-36 md:before:size-44 before:bg-gradient-to-tr before:from-[#8a66d6] before:to-[#4141e2] before:-ml-[100px] before:-mt-[80px] before:blur-[0.5px] before:rounded-full before:absolute" >
+          <h1 className="text-center my-6 text-white text-3xl font-bold">Sign up</h1>
           <Input type="text" label="Email" placeholder="Enter email" />
           <Input type="password" label="Password" placeholder="Enter password" />
           <Input type="password" label="Password confirmation" placeholder="Re enter password" />
-            <NavLink className="text-blue-700 underline" to="/">Back to home</NavLink>
-          <Button className="w-full mx-auto mb-8 mt-4 py-4 px-11 bg-white border-2 rounded-xl border-white cursor-pointer text-black text-xl font-bold hover:border-black">Sign up</Button>
+          <NavLink className="text-blue-700 text-sm md:text-base underline" to="/">Back to home</NavLink>
+          <Button className="w-full mx-auto mb-4 md:mb-8 mt-4 py-3 md:py-4 bg-white border-1 md:border-2 rounded-xl border-white cursor-pointer text-black text-base md:text-xl font-bold hover:border-black">Sign up</Button>
         </form>
       </div>
     </div>
